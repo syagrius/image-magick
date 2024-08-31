c7a4cf2711394ce3f313abfe2dddf3b6cbd5a64c
Syagrius <syagrius@pm.me>
2024-08-31 21:31:43 +0200

    Demo with colors, resampling with fixed height or width

--- demo/UMain.pas
+++ demo/UMain.pas
@@ -5,17 +5,33 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects, FMX.ListBox;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects, FMX.ListBox, FMX.Edit,
+  FMX.EditBox, FMX.SpinBox;
 
 type
   TForm1 = class(TForm)
     Button1: TButton;
     Image1: TImage;
     GravityCombo: TComboBox;
+    ColorsCombo: TComboBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    SampleWidthEd: TSpinBox;
+    SampleHeightEd: TSpinBox;
+    Label5: TLabel;
+    CanvasWidthEd: TSpinBox;
+    Label6: TLabel;
+    CanvasHeightEd: TSpinBox;
+    AdjHeightRb: TRadioButton;
+    AdjWidthRb: TRadioButton;
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure GravityComboChange(Sender: TObject);
+    procedure AdjHeightRbChange(Sender: TObject);
+    procedure ColorsComboChange(Sender: TObject);
   private
     { Private declarations }
     procedure RedrawFromGui;
@@ -35,10 +51,21 @@ uses
 
 {$R *.fmx}
 
+procedure TForm1.AdjHeightRbChange(Sender: TObject);
+begin
+  RedrawFromGui;
+end;
+
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   RedrawFromGui;
 end;
+
+procedure TForm1.ColorsComboChange(Sender: TObject);
+begin
+  RedrawFromGui;
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   // Is necessary to load/unload the dll manually. The dll load is dynamic/manual
@@ -70,7 +97,21 @@ begin
   //LLiteBitmap := TipImageMagickLiteBitmap.Create(TFile.ReadAllBytes('.\photos\piclumen-1724960425471.png'), TRect.Create(372, 159, 612, 399));
 
   Ratio := LLiteBitmap.Width / LLiteBitmap.Height;
-  LLiteBitmap.Resize(Round(800 * Ratio), 800, TipScaleAlgorithm.Lanczos,1000,900, GravityType(GravityCombo.ItemIndex), 'yellow');
+
+  if AdjHeightRb.IsChecked then
+    LLiteBitmap.Resize(Round(SampleHeightEd.Value / Ratio),
+    Trunc(SampleHeightEd.Value),
+    TipScaleAlgorithm.Lanczos,
+    Trunc(CanvasWidthEd.Value), Trunc(CanvasHeightEd.Value),
+    GravityType(GravityCombo.ItemIndex), ColorsCombo.Items[ColorsCombo.ItemIndex])
+  else
+    LLiteBitmap.Resize(
+    Trunc(SampleWidthEd.Value),
+    Round(SampleWidthEd.Value / Ratio),
+    TipScaleAlgorithm.Lanczos,
+    Trunc(CanvasWidthEd.Value), Trunc(CanvasHeightEd.Value),
+    GravityType(GravityCombo.ItemIndex), ColorsCombo.Items[ColorsCombo.ItemIndex]);
+
 
   TFile.WriteAllBytes('..\..\..\photos\output_image.png', LLiteBitmap.ToBytes(TipImageFormat.Jpg, 90));
   //Showmessage(Format('Saved "%s"!', [TPath.GetFullPath('..\..\..\output_image.png')]));
